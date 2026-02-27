63cc8a187f82c7b76bac3d3f5a4c7622cbd53e70
Oluwatooni Adebayo <jadebayo@andrew.cmu.edu>
2026-02-27 23:41:16 +0200

    Fix: switch to @neondatabase/serverless

--- netlify/functions/submit-survey.js
+++ netlify/functions/submit-survey.js
@@ -1,6 +1,6 @@
-import { neon } from "@netlify/neon";
+import { neon } from "@neondatabase/serverless";
 
-const sql = neon(); // automatically uses NETLIFY_DATABASE_URL
+const sql = neon(process.env.NETLIFY_DATABASE_URL);
 
 export const handler = async (event) => {
   if (event.httpMethod !== "POST") {
